150354a2fe6aba95947e4ce0583840df51b32098
Dalia <daliasawaya@tests-MacBook-Air-2.local>
2018-01-14 23:06:46 -0800

    reverse previous edits

--- make_america_great_client/src/index.js
+++ make_america_great_client/src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import store from './store.js'
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+	<App />, 
+	document.getElementById('root')
 );
 
 registerServiceWorker();
