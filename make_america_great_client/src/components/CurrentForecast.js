818a360477aa70669c3e62286583d93885f3013a
Dalia <daliasawaya@tests-MacBook-Air-2.local>
2018-01-21 18:13:55 -0800

    edit presentation of weather data in CurrentForecast component

--- make_america_great_client/src/components/CurrentForecast.js
+++ make_america_great_client/src/components/CurrentForecast.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const CurrentForecast = ({forecast: {temperature}}) => <div> <h1> Current Forecast: </h1>
+const CurrentForecast = ({forecast: {temperature}}) => <div>
 
 		<div style={{ border: 'solid 1px blue', padding: '20px', margin: '20px' }}>
-		<h3> Temperature: { temperature } farenheit </h3>
+		<h2> Current Temperature: </h2>
+		<h3> { temperature } &#176; farenheit </h3>
 		</div>
 
 	</div>
