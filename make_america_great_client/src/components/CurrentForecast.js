d5adcee9ae5b312d455f9cfa1dd2ae23909f9463
Dalia <daliasawaya@tests-MacBook-Air-2.local>
2018-01-22 02:03:10 -0800

    minor edits to currentforecast component text

--- make_america_great_client/src/components/CurrentForecast.js
+++ make_america_great_client/src/components/CurrentForecast.js
@@ -4,7 +4,7 @@ const CurrentForecast = ({forecast: {temperature}}) => <div>
 
 		<div style={{ border: 'solid 1px blue', padding: '20px', margin: '20px' }}>
 		<h2> Current Temperature: </h2>
-		<h3> { temperature } &#176; farenheit </h3>
+		<h3> { Math.round(temperature) } &#176; F </h3>
 		</div>
 
 	</div>
