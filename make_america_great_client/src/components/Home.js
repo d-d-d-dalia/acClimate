33980052aec4b6b82626c19c1e8f9316776119b0
Dalia <daliasawaya@tests-MacBook-Air-2.local>
2018-02-02 00:55:15 -0500

    edit views in home, howitworks and add to scratch file

--- make_america_great_client/src/components/Home.js
+++ make_america_great_client/src/components/Home.js
@@ -1,11 +1,7 @@
 import React from 'react';
-
+//this is the header. it is poorly named "Home"
 const Home = () => {
   return (
-    // <div className='Home'>
-    //   <h1>Make America Great</h1>
-    //   <p>one degree at a time</p>
-    // </div>
     <div className="App-header">
 		<h1> acClimate </h1>
 		<h5> make america great, one degree at a time </h5>
