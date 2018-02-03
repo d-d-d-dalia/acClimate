35b77562b10f626d49abbcc6fa440d9fef67bc21
Dalia <daliasawaya@tests-MacBook-Air-2.local>
2018-02-03 16:13:17 -0500

    howitworks language edit

--- make_america_great_client/src/components/howItWorks.js
+++ make_america_great_client/src/components/howItWorks.js
@@ -11,8 +11,8 @@ const howItWorks = () => {
       <p> acClimate is a React app using Redux middleware, connected to a Rails api backend. 
       it pulls temperature data for your current location using the darksky.net api. </p>
 
-      <p> it compares your guess to the actual temperature reading and evaluates it as true if it's within 2 degrees of accuracy.
-      the guesses log records your guesses along with the actual temperature reading. </p>
+      <p> acClimate compares your guess to the actual temperature reading and evaluates it as true if it's within 2 degrees of accuracy.
+      the guesses log records your guesses along with the actual temperature, for a side by side comparison. </p>
 	  <Link to={`/guesses`} > <h5> guesses log </h5> </Link>
 	  <Link to={`/about`} > <h5> the purpose </h5> </Link>
 	  <Link to={`/`} > <h5> home </h5> </Link>
