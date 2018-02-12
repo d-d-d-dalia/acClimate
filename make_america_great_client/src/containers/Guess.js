ebe635a52d6885f5e8bf555a806812ec14e3b602
Dalia <daliasawaya@tests-MacBook-Air-2.local>
2018-02-12 17:27:35 -0500

    use bind instead of arrow syntax in guess.js container

--- make_america_great_client/src/containers/Guess.js
+++ make_america_great_client/src/containers/Guess.js
@@ -10,9 +10,13 @@ class Guess extends Component {
     this.state = {
       count: 0,
     }
+
+    //this.handleOnLike = this.handleOnLike.bind(this)
   }
 
-handleOnLike = () => {
+// instead of using the arrow function fancy syntax which automatically associates "this" to the scope of the component, we are instead using bind on line 29
+// to explicitly define what "this" is here, i.e. the guess component
+handleOnLike ()  {
   this.setState({count: this.state.count + 1});
 }
 
@@ -25,7 +29,7 @@ handleOnLike = () => {
         <div key={guess.id}>
            <p> {guess.date} </p>
            <p> guess: {guess.guess} ~~ actual: {Math.round((guess.temperature -32) / 1.8)} </p>
-           <button type="submit" onClick={(event) => this.handleOnLike(event)}> like {this.state.count} </button> 
+           <button type="submit" onClick={this.handleOnLike.bind(this)}> like {this.state.count} </button> 
         </div>
     )
   }
